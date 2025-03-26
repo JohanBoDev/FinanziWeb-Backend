4be3d7c448406bd0d8ee50f99bcdc5a1e1474e4a
JohanBoDev <bohorquezjohan958@gmail.com>
2025-03-26 16:09:51 -0500

    z

--- Backend/routes/investmentRoutes.js
+++ Backend/routes/investmentRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const investmentController = require("../controllers/investmentController");
-const { verificarToken } = require("../middleware/auth");
+const { verificarToken, verificarTokenOpcional } = require("../middleware/auth");
 
 // 📌 Rutas CRUD para Cálculos de Inversión
-router.post("/", verificarToken, investmentController.createInvestment);  // Crear un cálculo de inversión
+router.post("/", verificarTokenOpcional, investmentController.createInvestment);  // Crear un cálculo de inversión
 router.get("/", verificarToken, investmentController.getInvestments);     // Obtener todos los cálculos del usuario
 router.get("/:id", verificarToken, investmentController.getInvestmentById); // Obtener un cálculo por ID
 router.put("/:id", verificarToken, investmentController.updateInvestment); // Actualizar un cálculo
